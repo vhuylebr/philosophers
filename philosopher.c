a43536ab17999154a5872321e12e6653c38dfeb1
Sully <sully.pol@epitech.eu>
2018-03-03 13:49:29 +0100

    help

--- philosopher.c
+++ philosopher.c
@@ -6,10 +6,24 @@
 */
 
 #include "extern.h"
+#include "philosopher.h"
+#include "string.h"
+
+void help()
+{
+	printf("USAGE\n\t./philo -p nbr_p -e nbr_e\n\nDESCRIPTION\n\t");
+	printf("-p nbr_p number of philosophers\n\t");
+	printf("-e nbr_e maximum number times a philosopher ");
+	printf("eats before exiting the program\n");
+}
 
 int main(int ac, char **av)
 {
-	lphilo_sleep();
-	lphilo_think();
-        return 0;
+	if (strcmp(av[1], "--help") == 0)
+		help();
+	if (ac != 5)
+		return (84);
+	if (atoi(av[2]) <= 0)
+		return (84);
+        return (0);
 }
